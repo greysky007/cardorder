e6868da45c35d92a45106068e045f0ad181e0ccc
greysky <antonysnork@gmail.com>
2022-09-17 19:10:51 +0300

    fix11

--- src/test/java/ru/netology/cardorder/CallbackTest.java
+++ src/test/java/ru/netology/cardorder/CallbackTest.java
@@ -1,4 +1,5 @@
 package ru.netology.cardorder;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,6 +13,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
+
+import static java.nio.channels.FileChannel.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
@@ -19,16 +22,16 @@ public class CallbackTest {
 
     @BeforeAll
     static void setupAll() {
-       // System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        // System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
 
     }
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+
         ChromeOptions options = new ChromeOptions();
-       options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
@@ -50,7 +53,20 @@ public class CallbackTest {
         driver.findElement(By.cssSelector(".button__text")).click();
         String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-
-
     }
 }
+   /*@Test
+   void shouldSubmitRequest() {
+       open("http://localhost:9999");
+       SelenideElement form = $("[data-test-id=callback-form]");
+       form.$("[data-test-id=name] input").setValue("Василий");
+       form.$("[data-test-id=phone] input").setValue("+79270000000");
+       form.$("[data-test-id=agreement]").click();
+       form.$("[data-test-id=submit]").click();
+       $(".alert-success").shouldHave(exactText("Ваша заявка успешно
+               отправлена!"));
+   }*/
+
+
+
+
